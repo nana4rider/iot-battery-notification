23524d79772b4780e4bde018f9d7ddaa7f337071
Shunichiro Aki <register.develop@nana4.net>
2021-08-31 23:10:25 +0900

    Flicの複数母艦に対応

--- src/task/FlicTask.ts
+++ src/task/FlicTask.ts
@@ -5,53 +5,69 @@ import { BaseTask, BatteryInfo } from './BaseTask';
 
 const logger = log4js.getLogger();
 
+type FlicConfig = {
+  hosts: string[]
+  deviceNames: Record<string, string>
+};
+
 const batteryInfoMap: Record<number, BatteryInfo> = {
   1: { type: 'cr2016', quantity: 1 },
   2: { type: 'cr2032', quantity: 1 }
 };
 
 export class FlicTask extends BaseTask {
-  private client;
-
   constructor() {
     super('Flic');
-    this.client = new FlicClient(config.get('flic.host'));
-  }
-
-  private async getButtons() {
-    return new Promise<string[]>(resolve => {
-      this.client.once('ready', () => {
-        this.client.getInfo(info => resolve(info.bdAddrOfVerifiedButtons));
-      });
-    });
   }
 
   async process(finish: () => void): Promise<void> {
-    const buttons = await this.getButtons();
-    const deviceNames: Record<string, string> = config.get('flic.deviceNames');
+    const flicConfig: FlicConfig = config.get('flic');
+    const deviceNames = flicConfig.deviceNames;
 
-    let found = 0;
-    for (const macAddress of buttons) {
-      this.client.getButtonInfo(macAddress, (bdAddr: string, uuid: string, color: string,
-        serialNumber: string, flicVersion: number, firmwareVersion: number) => {
+    let finishClientCount = 0;
+    const finishClient = () => {
+      if (++finishClientCount === flicConfig.hosts.length) {
+        finish();
+      }
+    };
 
-        const fsl = new FlicBatteryStatusListener(macAddress);
-        this.client.addBatteryStatusListener(fsl);
-        fsl.on('batteryStatus', (batteryPercentage: number, timestamp: Date) => {
-          logger.debug('Flic Timestamp:', serialNumber, timestamp);
+    for (const host of flicConfig.hosts) {
+      let finishButtonCount = 0;
+      const client = new FlicClient(host);
 
-          this.addStatus({
-            id: macAddress,
-            deviceName: deviceNames[serialNumber] ?? serialNumber,
-            value: batteryPercentage,
-            info: this.getBatteryInfo(flicVersion)
+      client.once('ready', () => client.getInfo(info => {
+        const buttons = info.bdAddrOfVerifiedButtons;
+        if (!buttons.length) {
+          finishClient();
+          return;
+        }
+
+        for (const macAddress of buttons) {
+          client.getButtonInfo(macAddress, (bdAddr: string, uuid: string, color: string,
+            serialNumber: string, flicVersion: number, firmwareVersion: number) => {
+
+            const fsl = new FlicBatteryStatusListener(macAddress);
+            client.addBatteryStatusListener(fsl);
+
+            fsl.on('batteryStatus', (batteryPercentage: number, timestamp: Date) => {
+              logger.debug('Flic Timestamp:', serialNumber, timestamp);
+
+              this.addStatus({
+                id: macAddress,
+                deviceName: deviceNames[serialNumber] ?? serialNumber,
+                value: batteryPercentage,
+                info: this.getBatteryInfo(flicVersion)
+              });
+
+              client.removeBatteryStatusListener(fsl);
+
+              if (++finishButtonCount === buttons.length) {
+                finishClient();
+              }
+            });
           });
-          this.client.removeBatteryStatusListener(fsl);
-          if (++found >= buttons.length) {
-            finish();
-          }
-        });
-      });
+        }
+      }));
     }
   }
 
